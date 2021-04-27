158bb6f026991499b1a85756160b454a4eee2522
Hendrik Bulens <hendrik.bulens@gmail.com>
2021-04-27 11:12:45 +0200

    Add attachments for SendGrid mailer #7

---
+++ src/Facteur.SendGrid/AttachmentConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using SendGrid.Helpers.Mail;
+
+namespace Facteur.SendGrid
+{
+    internal static class AttachmentConverter
+    {
+        internal static SendGridMessage AddAttachments(this SendGridMessage message, EmailRequest request)
+        {
+            message.Attachments = request.Attachments.Select(x => new global::SendGrid.Helpers.Mail.Attachment
+            {
+                Content = Convert.ToBase64String(x.ContentBytes),
+                Filename = x.Name,
+                Disposition = "attachment"
+            }).ToList();
+
+            return message;
+        }
+    }
+}
